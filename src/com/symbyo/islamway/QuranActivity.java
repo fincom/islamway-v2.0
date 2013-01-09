29ad3b8964021443e7b4d8b3ae8e18c924494589
Khalid Eldehairy <kdehairy@symbyo.com>
2013-01-09 12:32:39 +0200

    Integrated ActionbarSherlock and SlidingMenu.

--- src/com/symbyo/islamway/QuranActivity.java
+++ src/com/symbyo/islamway/QuranActivity.java
@@ -2,15 +2,24 @@ package com.symbyo.islamway;
 
 import android.os.Bundle;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class QuranActivity extends SherlockFragmentActivity {
+public class QuranActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+    	getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    	
         setContentView(R.layout.activity_quran);
+        setBehindContentView(R.layout.activity_quran);
+        
+        SlidingMenu sMenu = getSlidingMenu();
+        sMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
     }
 
     @Override
@@ -18,6 +27,15 @@ public class QuranActivity extends SherlockFragmentActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
     	getSupportMenuInflater().inflate(R.menu.activity_quran, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case android.R.id.home:
+    		toggle();
+    	}
+    	return super.onOptionsItemSelected(item);
     }
     
 }
